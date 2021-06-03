0ea218003cc5d98a152d22dd628b5ec29d875b95
Nicolás Arias <55033621+SrDeWitt1912@users.noreply.github.com>
2021-06-03 15:36:04 -0500

    Add files via upload

---
+++ Program01.cpp
@@ -0,0 +1,27 @@
+/*************************************************************************************************************
+A program with some functions and comments
+***************************************************************************************************************/
+
+#include <iostream>
+using namespace std;
+
+void line(), message();
+
+int main() {
+	cout << "Hello! The program starts in main()."
+		<< endl;
+	line();
+	message();
+	line();
+	cout << "At the end of main()." << endl;
+
+	return 0;
+}
+
+void line() {
+	cout << "------------------------------------------------------------" << endl;
+}
+
+void message() {
+	cout << "In function message()." << endl;
+}
