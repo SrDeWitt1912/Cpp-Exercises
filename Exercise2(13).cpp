0ce6ed415a336f1caeb9793d88a828238f76c2e1
Nicolás Arias <55033621+SrDeWitt1912@users.noreply.github.com>
2021-06-03 15:51:27 -0500

    Add files via upload
    Exercise 2 Section 13

---
+++ Exercise2(13).cpp
@@ -0,0 +1,13 @@
+/* Now you should not forget your glasses */
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "If this text";
+	cout << " appears on your display, ";
+	cout << endl;
+	cout << "You can pat yourself on ";
+	cout << "the back!" << endl;
+
+	return 0;
+}
