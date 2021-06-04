b7458ba45d0403ba47362e031045a6beb16a47bb
Nicolás Arias <55033621+SrDeWitt1912@users.noreply.github.com>
2021-06-04 09:57:59 -0500

    Add files via upload

---
+++ Exercise1(53).cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+
+	float a = sqrt(4);
+	float b = sqrt(12.25);
+	float c = sqrt(0.0121);
+
+	cout << "Number\t\tSquare root" << endl;
+	cout << "--------------------------------------------\n";
+	cout << "4\t\t" << a << endl;
+	cout << "12.25\t\t" << b << endl;
+	cout << "0.0121\t\t" << c << endl;
+}
